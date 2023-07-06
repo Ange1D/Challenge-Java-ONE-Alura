b5eedc1259ae899fdcbd6fd20d590f8987120220
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-07-05 21:29:57 -0600

    Cuadro de diálogo - Menú Principal

---
+++ src/main/java/org/challenge/Main.java
@@ -0,0 +1,16 @@
+package org.challenge;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] opciones = new String[] {"Selecciona...", "Conversor de Moneda", "Conversor de Temperatura"};
+
+        String opcionSeleccionada = JOptionPane.showInputDialog(null,"Selecciona una opcion de conversion","Menu",
+                JOptionPane.PLAIN_MESSAGE,null,opciones,
+                opciones[0]).toString();
+
+        JOptionPane.showMessageDialog(null,"Selecciono "+opcionSeleccionada);
+    }
+}
