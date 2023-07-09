15589fd8aa45c3328c8b2c9dbc478424151b3356
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-07-08 19:12:15 -0600

    Cuadro de diálogo - Opciones de Monedas

--- src/main/java/org/challenge/conversorMonedas.java
+++ src/main/java/org/challenge/conversorMonedas.java
@@ -11,12 +11,21 @@ public class conversorMonedas {
         String cantidadIngresada = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que desea convertir");
         try {
             cantidad = Double.parseDouble(cantidadIngresada);
-
+            new conversorMonedas().menuMonedas();
         }
         catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Debes ingresar un numero", "Error", JOptionPane.ERROR_MESSAGE);
         }
         return cantidad;
+    }
+
+    public void menuMonedas() {
+        String[] monedas = new String[] {"Selecciona...", "De peso a Dolar", "De peso a Euro", "De peso a Libra", "De peso a Yen", "De peso a Won Coreano"
+                , "De Dolar a peso", "De Euro a peso", "De Libra a peso", "De Yen a peso", "De Won Coreano a peso"};
+
+        String opcionSeleccionada = JOptionPane.showInputDialog(null,"Selecciona la moneda que deseas convertir","Menu Monedas",
+                JOptionPane.PLAIN_MESSAGE,null,monedas,
+                monedas[0]).toString();
     }
 
 
