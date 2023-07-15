938cf4520aff2a8cb8228ed1ea9348bc9f5157ba
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-07-15 12:19:50 -0600

    refactor

---
+++ src/main/java/org/challenge/menu.java
@@ -0,0 +1,24 @@
+package org.challenge;
+
+import javax.swing.*;
+
+public class menu {
+
+    public menu(){
+        String[] opciones = new String[] {"Selecciona...", "Conversor de Moneda", "Conversor de Temperatura"};
+
+        String opcionSeleccionada = JOptionPane.showInputDialog(null,"Selecciona una opcion de conversion","Menu",
+                JOptionPane.PLAIN_MESSAGE,null,opciones,
+                opciones[0]).toString();
+
+
+        switch (opcionSeleccionada){
+            case "Conversor de Moneda": new conversorMonedas().menuMonedas(new conversorMonedas().cantidad());
+            case "Conversor de Temperatura": break;
+            default: JOptionPane.showMessageDialog(null,"No es una opcion valida ");
+
+        }
+    }
+
+
+}
